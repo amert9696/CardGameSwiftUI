506fdfa218b8f24484205f05cad5ceee28174e6e
AMG <alimertgulec38@gmail.com>
2022-03-29 19:49:06 +0300

    Initial Commit

---
+++ CardGameSwiftUI/CardGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameSwiftUIApp.swift
+//  CardGameSwiftUI
+//
+//  Created by Ali Mert Güleç on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardGameSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardGameSwiftUI
+//
+//  Created by Ali Mert Güleç on 29.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
